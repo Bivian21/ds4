35a19abf8e3a8d6c76b0c8fa1fa8ba5f53244389
Bivian Paris <b.paris@hotmail.es>
2025-10-28 17:11:50 -0500

    Subir Laboratorio2 con proyectos 21 y 22

---
+++ Laboratorio2/Laboratorio22/Laboratorio22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Laboratorio22
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            MyClass.Valor = 37;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+
+    public class MyClass
+    {
+        public static int Valor;
+    }
+}
