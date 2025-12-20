376a4f9ec26968dff43f84892d8539b86dcda8da
Bivian Paris <b.paris@hotmail.es>
2025-12-20 08:37:40 -0500

    Subiendo Laboratorio 17 y 18

---
+++ Laboratorio17/Laboratorio171/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+
+namespace Laboratorio171
+{
+    public partial class WebForm1 : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ConnectionStringSettings connString = ConfigurationManager.ConnectionStrings["ConexiónNorthwind"];
+            SqlConnection conexion = new SqlConnection(connString.ConnectionString);
+
+            using (SqlCommand cmd = new SqlCommand("SalesByCategory", conexion))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@CategoryName", SqlDbType.VarChar).Value = "Seafood";
+
+                 conexion.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            if (reader.Read())
+                            {
+                                GridV.DataSource = reader;
+                                GridV.DataBind();
+                            }
+                        }
+                    }
+                }
+
+        }
+    }
+}
